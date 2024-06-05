702c37c2bbaef88a814a8b5f153645ac44b3f3af
kojinwook <gojinug4@gmail.com>
2024-06-05 10:18:25 +0900

    dasd

---
+++ a-private-project/src/main/java/com/example/a_private_project/APrivateProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.a_private_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APrivateProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(APrivateProjectApplication.class, args);
+	}
+
+}
---
+++ a-private-project/src/test/java/com/example/a_private_project/APrivateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.a_private_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class APrivateProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
